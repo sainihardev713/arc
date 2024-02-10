1cc027e6d4ac569a2ba873e2292f2a394f7e8de3
sainihardev713 <sainihardev713@gmail.com>
2024-02-10 15:16:02 +0530

    Update files by dell Sat Feb 10 15:16:02 IST 2024

---
+++ gitclone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello, this is a shell script!"
+echo "Running a command in the script:"
+
+# Your command here
+ls -l
+
+cd ..
+git clone https://github.com/sainihardev713/arc.git
+
+read -p "Press Enter to exit..."
